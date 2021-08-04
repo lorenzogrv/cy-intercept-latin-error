b5e2eba2ffdcc571f83e05803d553cc9b9216464
Lorenzo García Rivera <lorenzogrv@mareagestion.com>
2021-08-04 10:10:39 +0200

    [RED] First comit with two failing cases

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a reproducible example for an issue with [Cypress](cypress.io)
+
+```shell
+npm ci
+npm run test
+# or
+npm run test:ui
+```
+
+## Links
+
+- Current issue → https://github.com/cypress-io/cypress/issues/17055
+- Old issue → https://github.com/cypress-io/cypress/issues/16292
+
+
---
+++ cypress/integration/intercept-latin.spec.js
@@ -0,0 +1,67 @@
+const { cy, describe, it, beforeEach, expect } = global
+
+console.log(global)
+
+const URL = 'https://example.com/test'
+
+function request (body) {
+  /* global XMLHttpRequest */
+  cy.window().then(() => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('PUT', URL)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.send(JSON.stringify(body))
+  })
+}
+
+function dryAssert (interception) {
+  const { request: req } = interception
+  expect(req.body).to.not.be.an('ArrayBuffer')
+  expect(req.body).to.be.an('object')
+}
+
+describe('cy.intercept latin tilde ArrayBuffer issue', () => {
+  beforeEach(() => {
+    cy.intercept(URL, { body: { result: 'ok' } }).as('interception')
+  })
+
+  describe('Cases working as of 8.x', () => {
+    it('should work single "letter with tilde"', () => {
+      request({ key: 'á' })
+      cy.wait('@interception').then(dryAssert)
+    })
+    it('should work with five "letters with tilde"', () => {
+      request({ key: 'áéíóú' })
+      cy.wait('@interception').then(dryAssert)
+    })
+    it('should work with six "letters with tilde"', () => {
+      request({ key: 'áéíóúá' })
+      cy.wait('@interception').then(dryAssert)
+    })
+    it('should work with ten "letters with tilde"', () => {
+      request({ key: 'áéíóúáéíóú' })
+      cy.wait('@interception').then(dryAssert)
+    })
+    it('should work with fiveteen "letters with tilde"', () => {
+      request({ key: 'áéíóúáéíóúáéíóú' })
+      cy.wait('@interception').then(dryAssert)
+    })
+
+    it('should work with letters with tilde" within key', () => {
+      request({ 'tíldéd Kéy': null })
+      cy.wait('@interception').then(dryAssert)
+    })
+  })
+
+  describe('Cases not working as of 8.x', () => {
+    it('should work with mixed tilded and non-tilted letters', () => {
+      request({ key: 'válúéwíthsómánytíldés' })
+      cy.wait('@interception').then(dryAssert)
+    })
+
+    it('should work with letters with tilde" within key and value', () => {
+      request({ 'tíldéd Kéy': 'áéíóúáéíóúáéíóú' })
+      cy.wait('@interception').then(dryAssert)
+    })
+  })
+})
